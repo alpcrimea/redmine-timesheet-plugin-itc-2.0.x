afc835fea84b15ec1ebff7dad5ba2c3c1a246d6b
Craig Barber <CraigBarber@taryx.com>
2015-06-15 17:40:50 -0400

    * Updated - switched to CSV instead of FCSV

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -150,7 +150,7 @@ class Timesheet
 
   def to_csv
     out = "";
-    FCSV.generate(out, :encoding => 'u', :force_quotes => true) do |csv|
+    CSV.generate(out, :encoding => 'u', :force_quotes => true) do |csv|
       csv << csv_header
 
       # Write the CSV based on the group/sort
